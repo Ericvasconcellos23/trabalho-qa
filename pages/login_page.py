fd88e9ce6b34276c38dd9d0bfc30661904ad6481
Ericvasconcellos23 <vasconcelloseric23@gmail.com>
2026-08-03 22:04:30 -0300

    Adiciona testes de login invalido

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,39 @@
 from playwright.sync_api import Page
+from pages.produtos_page import ProdutosPage
+from utils.constantes import URL
 
-class LoginPage(Page):
+class LoginPage:
     def __init__(self, page: Page):
         self.page = page
+        self.usuario = page.locator("#user-name")
+        self.senha = page.locator("#password")
+        self.botao_entrar = page.locator("#login-button")
+        self.mensagem_erro = page.locator("[data-test='error']")
 
-    def abrir(self):
-        self.page.goto("https://www.saucedemo.com/")
+    def acessar(self):
+        self.page.goto(URL)
+        self.page.wait_for_load_state("networkidle")
+
+    def mensagem_erro_visivel(self):
+        return self.mensagem_erro.is_visible()
+
+    def obter_mensagem_erro(self):
+        return self.mensagem_erro.text_content()
+
+
+
+    def digitar_usuario(self, usuario):
+        self.usuario.fill(usuario)
+
+    def digitar_senha (self, senha):
+        self.senha.fill(senha)
+
+    def clicar_entrar(self):
+        self.botao_entrar.click()
+        return ProdutosPage(self.page)
+
+
+    def fazer_login(self, usuario, senha):
+        self.digitar_usuario(usuario)
+        self.digitar_senha(senha)
+        return self.clicar_entrar()
---
+++ tests/test_login_invalido.py
@@ -0,0 +1,35 @@
+from pages.login_page import LoginPage
+from utils.constantes import SENHA_VALIDA, USUARIO_VALIDO
+
+
+def test_login_com_usuario_invalido(pagina):
+    login = LoginPage(pagina)
+
+    login.acessar()
+
+    login.digitar_usuario("usuario_invalido")
+    login.digitar_senha(SENHA_VALIDA)
+
+    login.clicar_entrar()
+
+    assert login.mensagem_erro_visivel()
+
+    assert login.obter_mensagem_erro() == (
+        "Epic sadface: Username and password do not match any user in this service"
+    )
+
+
+def test_login_com_senha_invalida(pagina):
+    login = LoginPage(pagina)
+
+    login.acessar()
+    login.digitar_usuario(USUARIO_VALIDO)
+    login.digitar_senha("senha_errada")
+
+    login.clicar_entrar()
+
+    assert login.mensagem_erro_visivel()
+
+    assert login.obter_mensagem_erro() == (
+        "Epic sadface: Username and password do not match any user in this service"
+    )
