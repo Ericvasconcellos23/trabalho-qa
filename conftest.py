3d45f6b0dd05ccca766fd675034fb1517890d9b9
Ericvasconcellos23 <vasconcelloseric23@gmail.com>
2026-08-23 15:05:33 -0300

    Adiciona screenshot automático em falhas

--- conftest.py
+++ conftest.py
@@ -1,9 +1,10 @@
+
 import pytest
 import json
 from playwright.sync_api import sync_playwright
 
 @pytest.fixture
-def pagina():
+def pagina(request):
 
     with sync_playwright() as p:
         navegador = p.chromium.launch(headless=False)
@@ -12,12 +13,30 @@ def pagina():
 
         yield pagina
 
+        if hasattr(request.node, 'rep_call') and request.node.rep_call.failed:
+           nome_teste = request.node.name
+
+           pagina.screenshot(
+               path=f"evidencias/{nome_teste}.png",
+                full_page=True
+           )
+
         navegador.close()
+
+@pytest.hookimpl( hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+
+    resultado = yield
+
+    rep = resultado.get_result()
+
+    if rep.when == "call":
+        setattr(item,"rep_call", rep)
 
 @pytest.fixture
 def usuario():
 
-    with open("fixtures/usuarios.json") as arquivo:
+    with open("fixtures/usuario.json") as arquivo:
         dados = json.load(arquivo)
-
+     
     return dados
