8633a486d95bb45048d45b16e34265774cc95333
Ericvasconcellos23 <vasconcelloseric23@gmail.com>
2026-08-23 15:39:13 -0300

    Adiciona evidencias de testes com screenshot e video

--- conftest.py
+++ conftest.py
@@ -1,7 +1,8 @@
-
 import pytest
 import json
+import os
 from playwright.sync_api import sync_playwright
+
 
 @pytest.fixture
 def pagina(request):
@@ -9,21 +10,43 @@ def pagina(request):
     with sync_playwright() as p:
         navegador = p.chromium.launch(headless=False)
 
-        pagina = navegador.new_page()
+        contexto = navegador.new_context(
+            record_video_dir="evidencias/failed"
+        )
+
+        pagina = contexto.new_page()
 
         yield pagina
 
-        if hasattr(request.node, 'rep_call') and request.node.rep_call.failed:
-           nome_teste = request.node.name
+        nome_teste = request.node.name
 
-           pagina.screenshot(
-               path=f"evidencias/{nome_teste}.png",
+        if hasattr(request.node, "rep_call") and request.node.rep_call.failed:
+
+            pagina.screenshot(
+                path=f"evidencias/failed/{nome_teste}.png",
                 full_page=True
-           )
+            )
+
+            contexto.close()
+
+        else:
+
+            pagina.screenshot(
+                path=f"evidencias/passed/{nome_teste}.png",
+                full_page=True
+            )
+
+            video_path = pagina.video.path()
+
+            contexto.close()
+
+            if os.path.exists(video_path):
+                os.remove(video_path)
 
         navegador.close()
 
-@pytest.hookimpl( hookwrapper=True)
+
+@pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
 
     resultado = yield
@@ -31,12 +54,13 @@ def pytest_runtest_makereport(item, call):
     rep = resultado.get_result()
 
     if rep.when == "call":
-        setattr(item,"rep_call", rep)
+        setattr(item, "rep_call", rep)
+
 
 @pytest.fixture
 def usuario():
 
     with open("fixtures/usuario.json") as arquivo:
         dados = json.load(arquivo)
-     
+
     return dados
